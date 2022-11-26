3c6e77be13629d7b2e99774ac5d16e2d5aa962a6
citymer <cityhunter8484@gmail.com>
2022-11-26 17:58:38 +0100

    icone centre d'interet

--- src/components/CentreInteret.jsx
+++ src/components/CentreInteret.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import bricolage from '../assets/bricolage.png'
+import jardinage from '../assets/jardinage.png'
 
 const CentreInteret = () => {
   return (
@@ -9,6 +11,14 @@ const CentreInteret = () => {
           Passionné par le deux roues vélo/vtt en club depuis toujours
         </li>
         <li className="li">Amateur de bricolage et jardinage</li>
+        <div className="alignLogo">
+          <div className="contentlogo ">
+            <img src={bricolage} alt="logo deejay" className="withlogo" />
+          </div>
+          <div className="contentlogo ">
+            <img src={jardinage} alt="logo deejay" className="withlogo" />
+          </div>
+        </div>
       </ul>
     </div>
   )

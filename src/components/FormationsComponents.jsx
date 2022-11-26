3340ab2f796d67095c354c1faa8cb086b19d5d84
citymer <cityhunter8484@gmail.com>
2022-11-26 14:41:09 +0100

    image des projets

--- src/components/FormationsComponents.jsx
+++ src/components/FormationsComponents.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import devlogo from '../assets/devlogo.png'
+import habilitation from '../assets/habilitation.png'
 
 const FormationsComponents = () => {
   return (
@@ -9,9 +11,15 @@ const FormationsComponents = () => {
           2022 - OpenClassrooms - Développeur d'application {''}
           <strong>Javascript React</strong>
         </li>
+        <div className="contentlogo">
+          <img src={devlogo} alt="logo developpeur" />
+        </div>
         <li className="li">
           2019 - CFM - Habilitation électrique <strong>bo-ho-hov</strong>
         </li>
+        <div className="contentlogo">
+          <img src={habilitation} alt="logo developpeur" />
+        </div>
       </ul>
     </div>
   )
--- src/components/ProjetsRealises.jsx
+++ src/components/ProjetsRealises.jsx
@@ -3,6 +3,15 @@ import reservia from '../assets/reservia.PNG'
 import github from '../assets/GitHub-LogoPNG1.png'
 import website from '../assets/logo-websites-31315.png'
 import ohmyfood from '../assets/ohmyfood.PNG'
+import fisheyes from '../assets/fisheyes.PNG'
+import petitsplats from '../assets/les petits plats.PNG'
+import maquette from '../assets/maquetteFigma.PNG'
+import kasa from '../assets/kasa.PNG'
+import sporsee from '../assets/sporsee.PNG'
+import argentbank from '../assets/argentbank.PNG'
+import hrnet from '../assets/hrnet.PNG'
+import gameon from '../assets/gameOn.PNG'
+import billed from '../assets/billed.PNG'
 
 const ProjetsRealises = () => {
   return (
@@ -29,10 +38,11 @@ const ProjetsRealises = () => {
               href="https://citymer.github.io/JeromeMercier_2_09082021/"
               className="link"
             >
-              <img src={website} alt="lien github" className="website" />
+              <img src={website} alt="lien site" className="website" />
             </a>
           </div>
         </div>
+
         <li className="li">Dynamisez une page web avec des animations CSS</li>
         <div className="contentImg">
           <img src={ohmyfood} alt="projet 2" />
@@ -51,31 +61,184 @@ const ProjetsRealises = () => {
               href="https://citymer.github.io/JeromeMercier_3_10092021/"
               className="link"
             >
-              <img src={website} alt="lien github" className="website" />
+              <img src={website} alt="lien site" className="website" />
             </a>
           </div>
         </div>
+
         <li className="li">Créez une landing page Javascript</li>
+        <div className="contentImg">
+          <img src={gameon} alt="projet 2" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_4_07102021"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">
           Créez un site accessible pour une plateforme de photographes
         </li>
+        <div className="contentImg">
+          <img src={fisheyes} alt="projet 2" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_6_10112021"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://citymer.github.io/JeromeMercier_6_10112021/"
+              className="link"
+            >
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
+
         <li className="li">
           Développez un algorithme de recherche en Javascript
         </li>
+        <div className="contentImg">
+          <img src={petitsplats} alt="projet 2" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_7_02032022"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://citymer.github.io/JeromeMercier_7_02032022/"
+              className="link"
+            >
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">Débuggez et testez un SaaS RH</li>
+        <div className="contentImg">
+          <img src={billed} alt="projet 11" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/billed"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">
           Définissez les besoins pour une app de soutien scolaire
         </li>
+        <div className="contentImg">
+          <img src={maquette} alt="projet 2" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.figma.com/file/dN1UCf3B4ZMNoPybYw6nmM/Maquette-Learn%40Home?node-id=0%3A1&t=DQMkCjf2ErpOIPQF-0"
+              className="link"
+            >
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">
           Développez une application Web avec React et React Router
         </li>
+        <div className="contentImg">
+          <img src={kasa} alt="projet 11" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_11_28072022"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">
           Développez un tableau de bord d'analytics avec React
         </li>
+        <div className="contentImg">
+          <img src={sporsee} alt="projet 11" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_12_19082022"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">
           Utilisez une API pour un compte utilisateur bancaire avec React
         </li>
+        <div className="contentImg">
+          <img src={argentbank} alt="projet 11" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_13_21092022"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
         <li className="li">Faite passer une librairie jQuery vers React</li>
+        <div className="contentImg">
+          <img src={hrnet} alt="projet 11" />
+          <div className="contentLogo">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/citymer/JeromeMercier_14_11102022"
+              className="link"
+            >
+              <img src={github} alt="lien github" className="github" />
+            </a>
+            <a target="_blank" rel="noreferrer" href="d" className="link">
+              <img src={website} alt="lien site" className="website" />
+            </a>
+          </div>
+        </div>
       </ul>
     </div>
   )

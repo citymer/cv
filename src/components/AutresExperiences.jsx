287211326cee143676a14cca9d07b2f4a0f33ee8
citymer <cityhunter8484@gmail.com>
2022-11-26 17:48:57 +0100

    icones dans formations et autres experiences

--- src/components/AutresExperiences.jsx
+++ src/components/AutresExperiences.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import wine from '../assets/wine.png'
+import deejay from '../assets/deejay.png'
+import preparateur from '../assets/preparateur.png'
+import rayons from '../assets/rayons.png'
+import livreurs from '../assets/livreurs.png'
+import responsable from '../assets/responsable.png'
+import chefdeligne from '../assets/chefdeligne.png'
 
 const AutresExperiences = () => {
   return (
@@ -9,27 +16,53 @@ const AutresExperiences = () => {
           03/2017 - 04/2021 - Brotte SA - Chateauneuf du pape -{' '}
           <strong>Caviste</strong>
         </li>
+        <div className="contentlogo ">
+          <img src={wine} alt="logo developpeur" className="logo" />
+        </div>
         <li className="li">
           12/2013 - 02/2017 - JPM Events - Jonquières - <strong>Dee Jay</strong>
         </li>
+        <div className="contentlogo ">
+          <img src={deejay} alt="logo deejay" className="logo" />
+        </div>
         <li className="li">
           01/2012 - 12/2013 - Adecco - Orange -{' '}
           <strong>
             Préparateur de commandes, mise en rayon libre-service, livreur
           </strong>
         </li>
+        <div className="alignLogo">
+          <div className="contentlogo ">
+            <img src={preparateur} alt="logo deejay" className="withlogo" />
+          </div>
+          <div className="contentlogo ">
+            <img src={rayons} alt="logo deejay" className="withlogo" />
+          </div>
+          <div className="contentlogo ">
+            <img src={livreurs} alt="logo deejay" className="withlogo" />
+          </div>
+        </div>
         <li className="li">
           01/2009 - 01/2012 - Prestige et Tradition - Chateauneuf du pape -{' '}
           <strong>Responsable des commandes</strong>
         </li>
+        <div className="contentlogo ">
+          <img src={responsable} alt="logo developpeur" className="logo" />
+        </div>
         <li className="li">
           02/2006 - 10/2009 - SARL RAM'S - La Baume de transit -{' '}
           <strong>Dee Jay</strong>
         </li>
+        <div className="contentlogo ">
+          <img src={deejay} alt="logo deejay" className="logo" />
+        </div>
         <li className="li">
           01/1995 - 01/2008 - SAS Conserves de provence -{' '}
           <strong>Chef de ligne</strong>{' '}
         </li>
+        <div className="contentlogo ">
+          <img src={chefdeligne} alt="logo deejay" className="logo maxwith" />
+        </div>
       </ul>
     </div>
   )
